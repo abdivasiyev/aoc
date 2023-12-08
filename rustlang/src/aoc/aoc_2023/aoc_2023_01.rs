fe905248c80a1a51e42681373ae062fe2e1ad534
Asliddinbek Azizovich <46614434+abdivasiyev@users.noreply.github.com>
2023-12-08 15:25:38 +0500

    rust day 1

--- rustlang/src/aoc/aoc_2023/aoc_2023_01.rs
+++ rustlang/src/aoc/aoc_2023/aoc_2023_01.rs
@@ -18,21 +18,62 @@ impl aoclib::Solution for Aoc2023_01 {
 
     fn parse(&mut self) {
         let lines = aoclib::read_lines("input/2023-01.txt");
+        let nums = vec![
+            "one", "1",
+            "two", "2",
+            "three", "3",
+            "four", "4",
+            "five", "5",
+            "six", "6",
+            "seven", "7",
+            "eight", "8",
+            "nine", "9",
+        ];
 
         self.calibration_values = lines.iter().map(|line| {
             let mut calibration_value: i32 = 0;
 
-            let first = line.chars().into_iter().find(|c| c.is_ascii_digit()).map(|c| (c as i32) - ('0' as i32));
+            let first = line.chars().into_iter().enumerate().find_map(|(i, _)| {
+                for (j, num) in nums.iter().enumerate() {
+                    if i + num.len() > line.len() {
+                        continue;
+                    }
+
+                    let found = &line[i..i + num.len()];
+
+                    if *found == **num {
+                        return Some(j as i32 / 2 + 1);
+                    }
+                }
+
+                None
+            });
 
             match first {
-                None => {}
+                None => panic!("invalid input"),
                 Some(i) => calibration_value = calibration_value * 10 + i
             }
 
-            let second = line.chars().rev().into_iter().find(|c| c.is_ascii_digit()).map(|c| (c as i32) - ('0' as i32));
+            let second = line.chars().rev().into_iter().enumerate().find_map(|(i, _)| {
+                for (j, num) in nums.iter().enumerate() {
+                    if (line.len() as i32 - i as i32 - num.len() as i32) < 0 {
+                        continue;
+                    }
+
+                    let start = line.len() - i - num.len();
+                    let end = line.len() - i;
+                    let found = &line[start..end];
+
+                    if found == *num {
+                        return Some(j as i32 / 2 + 1);
+                    }
+                }
+
+                None
+            });
 
             match second {
-                None => {}
+                None => panic!("invalid input"),
                 Some(i) => calibration_value = calibration_value * 10 + i
             }
 
@@ -46,6 +87,7 @@ impl aoclib::Solution for Aoc2023_01 {
     }
 
     fn part_two(&mut self) -> String {
-        aoclib::output("unsolved")
+        let total: i32 = self.calibration_values.iter().sum();
+        aoclib::output(total)
     }
 }
