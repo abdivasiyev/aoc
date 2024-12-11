f10faaf3d7a631eacf8ed71185be89130865b7c9
Asliddinbek Azizovich <asliddin.abdivasiyev@gmail.com>
2024-12-11 16:31:09 +0500

    Year 2024 Day 9 Part 2

--- rustlang/src/aoc/aoc_2024/day_9.rs
+++ rustlang/src/aoc/aoc_2024/day_9.rs
@@ -2,7 +2,7 @@ use crate::aoclib;
 
 #[derive(Default)]
 pub struct Aoc2024_09 {
-    blocks: Vec<i64>,
+    blocks: String,
 }
 
 impl Aoc2024_09 {
@@ -24,56 +24,144 @@ impl aoclib::Solution for Aoc2024_09 {
             panic!("No input");
         }
 
-        let mut id: i64 = 0;
-
-        for (i, c) in lines[0].chars().enumerate() {
-            let num = c.to_digit(10).unwrap();
-
-            if i % 2 == 0 {
-                self.blocks.extend((0..num).map(|_| id));
-                id += 1;
-            } else {
-                self.blocks
-                    .extend((0..num).map(|_| -1).collect::<Vec<i64>>());
-            }
-        }
+        self.blocks = lines[0].clone();
     }
 
     fn part_one(&mut self) -> Vec<String> {
+        let mut disk: Vec<i64> = self
+            .blocks
+            .chars()
+            .filter(|c| c.is_digit(10))
+            .filter_map(|c| c.to_digit(10))
+            .map(|n| n as usize)
+            .enumerate()
+            .flat_map(|(i, n)| {
+                if i % 2 == 0 {
+                    vec![(i / 2) as i64; n]
+                } else {
+                    vec![-1 as i64; n]
+                }
+            })
+            .collect();
+
         let mut i: usize = 0;
-        let mut j: usize = self.blocks.len() - 1;
-        let mut blocks = self.blocks.clone();
+        let mut j: usize = disk.len() - 1;
 
         while i < j {
-            if blocks[i] != -1 {
+            if disk[i] != -1 {
                 i += 1;
                 continue;
             }
 
-            if blocks[j] == -1 {
+            if disk[j] == -1 {
                 j -= 1;
                 continue;
             }
 
-            blocks.swap(i, j);
+            disk.swap(i, j);
             i += 1;
             j -= 1;
         }
 
         let mut sum = 0;
 
-        for i in 0..blocks.len() {
-            if blocks[i] == -1 {
+        for i in 0..disk.len() {
+            if disk[i] == -1 {
                 continue;
             }
 
-            sum += blocks[i] as i64 * i as i64;
+            sum += disk[i] as i64 * i as i64;
         }
 
         aoclib::output(sum)
     }
 
     fn part_two(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        let mut disk: Vec<Block> = self
+            .blocks
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .map(|n| n as usize)
+            .enumerate()
+            .map(|(i, n)| Block::new(i, n))
+            .collect();
+
+        let mut j: usize = disk.len() - 1;
+
+        while j > 0 {
+            if disk[j].type_ == 1 {
+                j -= 1;
+                continue;
+            }
+
+            let mut i: usize = 0;
+
+            while i < j {
+                if disk[i].type_ == 0 {
+                    i += 1;
+                    continue;
+                }
+
+                if disk[i].type_ == 1 && disk[j].data.len() > disk[i].free_space {
+                    i += 1;
+                    continue;
+                }
+
+                let data = disk[j].data.clone();
+
+                disk[i].fill(&data);
+                disk[j].clear();
+                break;
+            }
+            j -= 1;
+        }
+
+        let sum = disk
+            .iter()
+            .flat_map(|b| b.data.iter().cloned().chain(std::iter::repeat(0).take(b.free_space)))
+            .enumerate()
+            .map(|(i, n)| if n != -1 { n * i as i64 } else { 0 })
+            .sum::<i64>();
+
+        aoclib::output(sum)
+    }
+}
+
+#[derive(Debug)]
+struct Block {
+    type_: usize,
+    free_space: usize,
+    data: Vec<i64>,
+}
+
+impl Block {
+    fn new(id: usize, size: usize) -> Self {
+        if id % 2 == 0 {
+            Self {
+                type_: 0,
+                free_space: 0,
+                data: vec![(id as i64) / 2; size],
+            }
+        } else {
+            Self {
+                type_: 1,
+                free_space: size,
+                data: Vec::new(),
+            }
+        }
+    }
+
+    fn fill(&mut self, data: &Vec<i64>) {
+        self.data.extend(data.iter());
+        self.free_space -= data.len();
+        if self.free_space == 0 {
+            self.type_ = 0;
+        }
+    }
+
+    fn clear(&mut self) {
+        self.free_space = self.data.len();
+        self.data.clear();
+        self.type_ = 1;
     }
 }
